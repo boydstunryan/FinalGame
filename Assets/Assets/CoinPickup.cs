2bbe78fa8d919e8a2a1908c6ab9dcea132a7f4cd
ryanb <boydstunryan@yahoo.com>
2019-04-10 21:10:21 -0600

    Coin pick up script

--- Assets/Assets/CoinPickup.cs
+++ Assets/Assets/CoinPickup.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public int coinValue;
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if(other.tag == "Player")
+        {
+            print("You've collect the coin! idiot");
+
+            ScoreManager.AddPoints(coinValue);
+
+            Destroy(gameObject);
+        }
     }
-}
+}    
