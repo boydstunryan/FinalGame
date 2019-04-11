41175f3812a5ab17b6c5ec032ca6812f168dc5ec
ryanb <boydstunryan@yahoo.com>
2019-04-10 21:30:07 -0600

    EnemyPatrol Script

--- Assets/Assets/EnemyPatrol.cs
+++ Assets/Assets/EnemyPatrol.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class EnemyPatrol : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed;
+    public bool moveRight;
 
-    // Update is called once per frame
-    void Update()
+    public Transform wallCheck;
+    public float wallCheckRadius;
+    public LayerMask whatIsWall;
+    private bool hittingWall;
+
+    private bool notAtEdge;
+    public Transform edgeCheck;
+
+    private void Update()
     {
-        
+        notAtEdge = Physics2D.OverlapCircle(edgeCheck.position, wallCheckRadius, whatIsWall);
+
+        hittingWall = Physics2D.OverlapCircle(wallCheck.position, wallCheckRadius, whatIsWall);
+
+        if (hittingWall || !notAtEdge)
+        {
+            moveRight = !moveRight;
+        }
+
+        if (moveRight) {
+            transform.localScale = new Vector3(-10f, 10f, 1f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+        if (moveRight)
+        {
+            transform.localScale = new Vector3(-10f, 10f, 1f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent <Rigidbody2D>().velocity.y);
+        }
+        else
+        {
+            transform.localScale = new Vector3(10f, 10f, 1f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
     }
 }
