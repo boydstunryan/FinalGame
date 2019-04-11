36398e499aae3b9763b51c5edca93fc1d87c85bc
ryanb <boydstunryan@yahoo.com>
2019-04-10 21:58:41 -0600

    Stuff that murders the player

--- Assets/Assets/KillPlayer.cs
+++ Assets/Assets/KillPlayer.cs
@@ -7,12 +7,14 @@ public class KillPlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        levelManager = FindObjectOfType<LevelManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if(other.name == "Player")
+        {
+            levelManager.RespawnPlayer();
+        }
     }
 }
