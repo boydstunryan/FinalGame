33ba225ec404a046dcb3c956e730dfd5270e051c
ryanb <boydstunryan@yahoo.com>
2019-04-24 17:37:08 -0600

    Cleaned up the folders a bit and added a main menu

---
+++ Assets/Assets/Script/ScoreManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int score;
+
+    private Text scoreText;
+
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+
+        score = 0;
+    }
+
+    void Update()
+    {
+        if (score < 0)
+            score = 0;
+
+        scoreText.text = " " + score;
+    }
+
+    public static void AddPoints (int pointsToAdd)
+    {
+        score += pointsToAdd;
+    }
+}
