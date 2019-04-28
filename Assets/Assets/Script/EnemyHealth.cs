7a0239798b2ec73b26836bed8bb79dbbdcb0c316
ryanb <boydstunryan@yahoo.com>
2019-04-28 00:30:21 -0600

    Health scripts for enemy and player.

---
+++ Assets/Assets/Script/EnemyHealth.cs
@@ -0,0 +1,66 @@
+﻿
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public float sinkSpeed = 2.5f;
+    BoxCollider boxCollider;
+    bool isDead;
+    bool isSinking;
+
+    ParticleSystem hitParticles;
+    Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        hitParticles = GetComponentInChildren<ParticleSystem>();
+        boxCollider = GetComponent<BoxCollider>();
+
+        currentHealth = startingHealth;
+    }
+
+    void Update()
+    {
+        if(isSinking)
+        {
+            transform.Translate(-Vector3.up * sinkSpeed * Time.deltaTime);
+        }
+    }
+
+    public void TakeDamage (int amount, Vector3 hitPoint)
+    {
+        if (isDead) return;
+
+        currentHealth -= amount;
+
+        hitParticles.transform.position = hitPoint;
+
+        if(currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+
+        boxCollider.isTrigger = true;
+
+        anim.SetTrigger("Dead");
+    }
+
+    public void StartSinking()
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+
+        isSinking = true;
+
+        //ScoreManager.score += ;
+
+        Destroy(gameObject, 2f);
+    }
+}
---
+++ Assets/Assets/Script/PlayerHealth.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+    public Image damageImage;
+    public float flashSpeed = 5f;
+    public Color flashColor = new Color(1f, 0f, 0f, 0.1f);
+
+    Animator anim;
+    PlayerController playerController;
+    PlayerShoot playerShoot;
+    bool isDead;
+    bool damaged;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerController = GetComponent<PlayerController>();
+        playerShoot = GetComponentInChildren<PlayerShoot>();
+
+        currentHealth = startingHealth;
+    }
+
+    private void Update()
+    {
+        if (damaged)
+        {
+            damageImage.color = flashColor;
+        }
+
+        else
+        {
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+
+        damaged = false;
+    }
+
+    public void TakeDamage (int amount)
+    {
+        damaged = true;
+
+        currentHealth -= amount;
+
+        healthSlider.value = currentHealth;
+
+        if (currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+
+        playerShoot.DestroyFinishedParticle();
+
+        anim.SetTrigger("die");
+
+        playerController.enabled = false;
+        playerShoot.enabled = false;
+    }
+}
